57645f42b146e314cc4792c93af10dda43e1ca70
Jenny Shalai <jenny.shalai@gmail.com>
2019-06-18 16:11:40 -0400

    add dictionary theory

--- python-tuple-dictionary-set.py
+++ python-tuple-dictionary-set.py
@@ -15,3 +15,29 @@ tuple.count(value)  # how many times value in the tuple
 tuple.index(value)  # index of the first time seen value
 
 
+
+dictionary = {key1: value1, key2: value2}
+dictionary[key] = value # create new key-value
+dictionary[key]         # return value for that key
+
+for key in dictionary:
+    print(key)
+
+for value in dictionary.values():
+    print(value)
+
+for key, value in dictionary.items():
+    print(key, value)
+
+for pair in dictionary.items():
+    print(pair)
+# print out key-value pairs as tuple:
+# (key1, value1)
+# (key2, value2)
+# (key3, value3)
+
+if key in dictionary:
+    # do something
+
+
+
