dbbd63c01587aa3055884c7329a278479a455e6d
Jenny Shalai <jenny.shalai@gmail.com>
2019-06-18 16:08:54 -0400

    add tuple theory

--- python-tuple-dictionary-set.py
+++ python-tuple-dictionary-set.py
@@ -1,3 +1,17 @@
 # TUPLES, DICTIONARY, SET
 
+a_tuple = 1, 2
+another_tuple = (1, 2)
+yet_another_tuple = tuple([1, 2])
+
+# tuple is immutable, new items can't be added,
+# but if the tuple's item is a mutable it can be extand:
+tuple = (1, [2,3])
+tuple[1].append(4) # (1,[2,3,4])
+
+tuple[index] # element on given index
+len(tuple) # number of elements in the tuple
+tuple.count(value)  # how many times value in the tuple
+tuple.index(value)  # index of the first time seen value
+
 
