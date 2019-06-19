6bc91e66bc59ee6fba7c2facd1bbd8d25a140c97
Jenny Shalai <jenny.shalai@gmail.com>
2019-06-19 19:50:58 -0400

    add function signature

--- python-functions.py
+++ python-functions.py
@@ -1,1 +1,11 @@
 # FUNCTIONS
+
+
+def name_on_the_function(parameter):
+    function body
+
+#(!) any parameters with default values are after any parameters that aren’t given default values
+# def my_function(n, divisor=2): and NOT def my_function(n=2, divisor):
+
+# parameter - name of a variable in a function definition
+# argument - value passed in to a function
